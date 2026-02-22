10c86c52284f4ca5dfeba13fcd5895fea8ef4a47
Rakhi6984 <r03322169@gmail.com>
2026-02-22 19:15:28 +0530

    Create odd even.cpp

---
+++ odd even.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+int n,a;
+cout<<"enter the number:";
+cin>>n;
+a=n%2;
+if(a==0)
+{
+    cout<<"number is even"<<endl;
+}
+else
+{
+    cout<<"number is odd"<<endl;
+}
+ return 0;
+}
