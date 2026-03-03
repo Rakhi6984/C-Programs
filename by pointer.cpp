e4c1066211dd15cd4987e169bd42ed1680159fbb
Rakhi6984 <r03322169@gmail.com>
2026-03-03 17:56:55 +0530

    Create by pointer.cpp

---
+++ by pointer.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+void swap(int*,int*);
+int main() {
+    int a,b;
+    cout<<"enter two numbers";
+    cin>>a>>b;
+    swap(&a,&b);
+    cout<<"after function call"<<a<<" "<<b;
+    return 0;
+}
+void swap(int*x,int*y)
+{
+    int z;
+    z=*x;
+    *x=*y;
+    *y=z;
+    cout<<"inside function"<<*x<<" "<<*y<<endl;
+}
