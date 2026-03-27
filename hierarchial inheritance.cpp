eee3503eb6658c7ab48065e0eda0f91f6891781a
Rakhi6984 <r03322169@gmail.com>
2026-03-27 16:36:24 +0530

    Add files via upload

---
+++ hierarchial inheritance.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class a
+{
+	public:
+		void display_a()
+		{
+			cout<<"a class function called"<<endl;
+		}
+};
+class b:public a
+{
+	public:
+		void display_b()
+		{
+			display_a();
+			cout<<"b class function called"<<endl;
+		}
+};
+class c:public a
+{
+	public:
+		void display_c()
+		{
+			display_a();
+				cout<<"c class function called"<<endl;
+
+		}
+};
+int main()
+{
+	b bb;
+	c cc;
+	bb.display_b();
+	cc.display_c();
+	return 0;
+}
